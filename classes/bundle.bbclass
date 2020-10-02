rauc/meta-rauc

commit 4e920aa9e80a4a31aeb6aa41dff983f2d0127955

classes: bundle: Avoid using img_fstype from previous iteration
Signed-off-by: Andreas Fischer <af@bantuX.com>

classes/bundle.bbclass
@@ -166,7 +166,6 @@ def write_manifest(d):
     import shutil
 
     machine = d.getVar('MACHINE')
-    img_fstype = d.getVar('RAUC_IMAGE_FSTYPE')
     bundle_path = d.expand("${BUNDLE_DIR}")
 
     bb.utils.mkdirhier(bundle_path)
@@ -208,6 +207,8 @@ def write_manifest(d):
 
         if slotflags and 'fstype' in slotflags:
             img_fstype = slotflags.get('fstype')
+        else
+            img_fstype = d.getVar('RAUC_IMAGE_FSTYPE')
 
         if imgtype == 'image':
             if slotflags and 'file' in slotflags:

commit 04ead4e4968a9fd97511d1bd105c372fc3b2a8c5

classes/bundle: fix missing colon in else condition
This issue was introduced by previous commit
4e920aa9e80a4a31aeb6aa41dff983f2d0127955 and makes class parsing fail.

Signed-off-by: Enrico Jorns <ejo@pengutronix.de>

classes/bundle.bbclass
@@ -207,7 +207,7 @@ def write_manifest(d):
 
         if slotflags and 'fstype' in slotflags:
             img_fstype = slotflags.get('fstype')
-        else
+        else:
             img_fstype = d.getVar('RAUC_IMAGE_FSTYPE')
 
         if imgtype == 'image':