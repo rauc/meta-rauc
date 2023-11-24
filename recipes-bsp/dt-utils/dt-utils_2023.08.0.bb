rauc/meta-rauc

commit 6c5faed98b7124e1f084280b0981da349f38c8d6

dt-utils: update to 2023.08.0
Signed-off-by: Enrico Jorns <ejo@pengutronix.de>

recipes-bsp/dt-utils/dt-utils_2023.08.0.bb
@@ -1,0 +1,3 @@
+require dt-utils.inc
+
+SRC_URI[sha256sum] = "15cda6fdbaaf08711e55b49db4479f3af48660b812d8d587ec9b3fffaa13a0f7"

recipes-bsp/dt-utils/dt-utils.inc
@@ -2,7 +2,7 @@ DESCRIPTION = "device-tree and barebox-related tools"
 HOMEPAGE = "http://git.pengutronix.de/?p=tools/dt-utils.git"
 SECTION = "base"
 LICENSE = "GPL-2.0-only"
-LIC_FILES_CHKSUM = "file://COPYING;md5=9ac2e7cff1ddaf48b6eab6028f23ef88"
+LIC_FILES_CHKSUM = "file://COPYING;md5=18d902a0242c37a4604224b47d02f802"
 DEPENDS = "udev"
 PR = "r0"
 

recipes-bsp/dt-utils/dt-utils_2021.03.0.bb
@@ -1,3 +1,0 @@
-require dt-utils.inc
-
-SRC_URI[sha256sum] = "36a56924e356250988315cd8761fde52832e6d4934323aca2827ff93fa12907f"

commit b9938b796549a27bc481bac668d7104e58265bec

dt-utils: update to 2023.11.0
This release fixes an issue when using barebox-state on systems with a
barebox state partition on the eMMC user partition.
Without this patch barebox-state searches for the state partition on
one of the eMMC boot (hardware) partitions.

Fixes: 6c5faed ("dt-utils: update to 2023.08.0")
Signed-off-by: Leonard Göhrs <l.goehrs@pengutronix.de>

recipes-bsp/dt-utils/dt-utils_2023.08.0.bb
@@ -1,3 +1,0 @@
-require dt-utils.inc
-
-SRC_URI[sha256sum] = "15cda6fdbaaf08711e55b49db4479f3af48660b812d8d587ec9b3fffaa13a0f7"

recipes-bsp/dt-utils/dt-utils_2023.11.0.bb
@@ -1,0 +1,3 @@
+require dt-utils.inc
+
+SRC_URI[sha256sum] = "d224d941c076c143f43d59cd7c6e1c522926064a31ac34a67720632ddecb6b53"